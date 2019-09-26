00be13f2692d917d37f7011e6102fb871e9c9e7a
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-26 22:21:28 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -2,6 +2,8 @@ class Cat
   # Use attr_accessor macro to create a setter and getter method for a cat's name
   attr_accessor :name
   
+  
+  # .meow method to output "meow!" when called on an instance of Cat
   def meow
     puts "meow!"
   end
